3ccf3371137b6cde67ce9878dbced0598fc980a9
Helvin <hjwc86@gmail.com>
2020-09-21 13:41:19 +0200

    Migration TypeScript

--- index.js
+++
@@ -1,9 +0,0 @@
-
-console.log('** Administration Hotel **');
-const { Presentation } = require("./presentation")
-const { Service } = require('./service');
-
-const service = new Service();
-const presentation = new Presentation(service);
-
-presentation.start();
